3cbf7a5a5623552b9650230cbf522cdbee5f064b
Estefano13 <aeencarnacionsegura1@sheffield.ac.uk>
2021-07-23 18:44:58 +0100

    ML projects on the MNIST dataset; linear techniques and neural net approaches respectively

---
+++ Proj_2_3_MNIST_linear_approach_and_Neural_Net/mnist/part1/kernel.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+### Functions for you to fill in ###
+
+
+
+def polynomial_kernel(X, Y, c, p):
+    """
+        Compute the polynomial kernel between two matrices X and Y::
+            K(x, y) = (<x, y> + c)^p
+        for each pair of rows x in X and y in Y.
+
+        Args:
+            X - (n, d) NumPy array (n datapoints each with d features)
+            Y - (m, d) NumPy array (m datapoints each with d features)
+            c - a coefficient to trade off high-order and low-order terms (scalar)
+            p - the degree of the polynomial kernel
+
+        Returns:
+            kernel_matrix - (n, m) Numpy array containing the kernel matrix
+    """
+    # YOUR CODE HERE
+    Kernel = (np.dot(X,Y.T)+c)**p
+    return Kernel
+    raise NotImplementedError
+
+
+
+def rbf_kernel(X, Y, gamma):
+    """
+        Compute the Gaussian RBF kernel between two matrices X and Y::
+            K(x, y) = exp(-gamma ||x-y||^2)
+        for each pair of rows x in X and y in Y.
+
+        Args:
+            X - (n, d) NumPy array (n datapoints each with d features)
+            Y - (m, d) NumPy array (m datapoints each with d features)
+            gamma - the gamma parameter of gaussian function (scalar)
+
+        Returns:
+            kernel_matrix - (n, m) Numpy array containing the kernel matrix
+    """
+    # YOUR CODE HERE
+    norm_X_sq = np.dot(X, X.T)
+    norm_Y_sq = np.dot(Y, Y.T)
+    norm_last_term = 2 * np.dot(X, Y.T)
+    Kernel = np.exp(-gamma * (norm_X_sq + norm_Y_sq - norm_last_term))
+    return Kernel
+    raise NotImplementedError
